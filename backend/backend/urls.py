e3d869cd167b174c2231ff604373911fad51bd6d
Edgar Alberto Ay May <edgar2aymay6@gmail.com>
2026-04-15 20:16:10 -0600

    fix: serve media files in production

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,10 +27,7 @@ urlpatterns = [
     path('api/', include('users.urls')),
 ]
 
-
 # ==============================
-# 📂 MEDIA FILES (SOLO DESARROLLO)
+# 📂 MEDIA FILES
 # ==============================
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
